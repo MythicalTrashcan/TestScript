ef108ca78c901e77188a02587efa3e58c657570a
MythicalTrashcan <79861302+MythicalTrashcan@users.noreply.github.com>
2023-02-23 21:53:48 -0500

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -3,7 +3,9 @@ local Humanoid = Player.Character.Humanoid
 local RootPart = Humanoid.RootPart
 
 game:GetService("RunService").RenderStepped:Connect(function ()
-    if game.Players.LocalPlayer.Character.Humanoid:GetState() == Enum.HumanoidStateType.Climbing then
-        RootPart.Velocity = Vector3.new(RootPart.Velocity.X, Humanoid.JumpPower * 1.1, RootPart.Velocity.Z)
-    end
+    Humanoid.Jumping:Connect(function(isJumping)
+        if isJumping then
+	        RootPart.Velocity = Vector3.new(RootPart.Velocity.X, Humanoid.JumpPower * 1.1, RootPart.Velocity.Z)
+        end
+    end)
 end)
