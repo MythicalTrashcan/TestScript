53c78ca080c292dc82d3cf5ee8c2500b3553fbfb
MythicalTrashcan <79861302+MythicalTrashcan@users.noreply.github.com>
2023-02-23 22:00:44 -0500

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -5,7 +5,7 @@ local RootPart = Humanoid.RootPart
 game:GetService("RunService").RenderStepped:Connect(function ()
     Humanoid.Jumping:Connect(function(isJumping)
         if isJumping then
-	    RootPart.Velocity = Vector3.new(RootPart.Velocity.X, Humanoid.JumpPower * 1.1, RootPart.Velocity.Z)
+	        RootPart.Velocity = Vector3.new(RootPart.Velocity.X, Humanoid.JumpPower * 1.1, RootPart.Velocity.Z)
         end
     end)
 end)
